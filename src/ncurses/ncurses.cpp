e34e22220ccf17b5ddc66655a48ac30b87ff39d2
Maxime Payant <maxime@pop-os.localdomain>
2021-01-23 15:42:34 +0100

    ncruses begin

--- src/ncurses/ncurses.cpp
+++ src/ncurses/ncurses.cpp
@@ -4,3 +4,23 @@
 ** File description:
 ** ncurses.cpp
 */
+
+#include "ncurses.hpp"
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+int MyGKrellmTerminal()
+{
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    initscr();
+    printw("%d\n", w.ws_row);
+    printw("%d col\n", w.ws_col);
+    headInfo();
+
+    refresh();
+    getch();
+    endwin();
+    return 0;
+}
